f207d3145fdf2ba965f1c83a7f04d168b7368689
DavidStinghen <davidstinghen@gmail.com>
2019-07-27 00:11:29 -0300

    fet: add global style - home and cart routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import GlobalStyle from "./styles/global";
+import Router from "./routes";
 
 function App() {
-  return <h1>Hello Word</h1>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Router />
+    </BrowserRouter>
+  );
 }
 
 export default App;
