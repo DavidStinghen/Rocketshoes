4829737e9f3c408e20941a8484866b131cbc5c35
DavidStinghen <davidstinghen@gmail.com>
2019-07-30 00:24:14 -0300

    feature: add toastify to execute errors

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,7 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
+import 'react-toastify/dist/ReactToastify.css';
 
-import background from "../assets/images/background.svg";
+import background from '../assets/images/background.svg';
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
@@ -30,6 +31,4 @@ export default createGlobalStyle`
   button {
     cursor: pointer;
   }
-
-
 `;
